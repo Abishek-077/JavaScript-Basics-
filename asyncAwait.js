75f78de28ff16e9b396419ae56c7eb63c17bb9a3
Abishek-077 <abishekxofficial987@gmail.com>
2025-03-21 20:49:14 +0545

    Enhance async/await examples with detailed explanations and error handling

--- asyncAwait.js
+++ asyncAwait.js
@@ -82,3 +82,130 @@ async function getData(){
     let result3= await fetchData();
     console.log(result3);
 }
+
+getData();
+
+// much more cleaner ,readable and easier to understand!!!!
+// so guys u might be wandering by is so that we have use async in function fetchedData() and function
+// getData() but not in result it is beacause async will be requesting to response to the fetched data 
+// where as  await will just wait for fetch data to response onlt then gives the result.
+
+// 3. how does acync and await work internally 
+
+async function mesaage(){
+    return "hello world";
+}
+
+console.log(example());
+// async always return the valur even its the simple value.
+
+
+// clear explanation of await
+
+async function metting(){
+    setTimeout(()=>{
+        console.log("Data is fetched");
+    },2000);
+};
+
+async function waiting(){
+
+    console.log("fetched data");
+    const person1 = await metting();
+    console.log (person1);
+
+    const person2 = await metting();
+    console.log(person2);
+
+    const person3 = await metting();
+    console.log(person3)
+}
+
+// i will be re writing the code 
+
+async function meeting(){
+    return new promise((resolve)=>{
+        setTimeout(()=>{
+            console.log("Google interview is ongoing");
+        },2000);
+        
+    });
+}
+
+async function person(){
+    console.log("Before fetching ")
+
+    let person1 = await meeting();
+    console.log(person1);
+
+    let person2 =await metting();
+    console.log(person2);
+
+    let person3 = await metting();
+    console.log(person3);
+
+    console.log("After feteching..........")
+}
+
+// WorkFlow of await 
+// 1. print("before fetching................")
+// 2. pause at await metting for 2 secs.
+// 3. prints "Data Fetched".
+// 4. Prints "After fetching............"
+
+// 5. Error Handling in async/await
+// also used in try,...catch to handel error peoperly
+
+async function fetchData(){
+    return new Promise((reject)=>{
+        setTimeout(()=>{
+            reject("something went wrong!");
+        },2000);
+    });
+}
+
+async function getData(){
+    try{
+        console.log("fetching")
+
+        let data = await fetchData();// this will throw an error
+        console.log(data);
+    }catch(error){
+        console.log("Error",error)
+    }
+}
+
+getData();
+
+// 6. Running multiple await call in parallel
+// instated of waiting one after another, we can run using Promise.all()
+
+async function fetchData(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("U are now memeber of elite group");
+        },2000);
+    });
+}
+
+async function getData(){
+    console.log("Pending the fetched data......");
+    const [data1,data2,data3,data4] = await Promise.all([
+        fetchData(1),
+        fetchData(2),
+        fetchData(3)
+    ]);
+
+    console.log(data1);
+    console.log(data2);
+    console.log(data3);
+    console.log(data4)
+
+
+}
+
+// so like in pervious example we had been calling it using async and await one by one 
+// but here we promise it to call all the data with promise.all([])
+
+//summary of async and await
+
